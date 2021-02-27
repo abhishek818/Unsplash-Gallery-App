9a4b6a6cafd4fd713906921c938f4aa1fc3e8925
Abhishek Kumar Gupta <abhishekguptaatweb17@gmail.com>
2021-02-27 22:27:27 +0530

    add popup modal

---
+++ Desktop/Tars-app/images/client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import ListImages from "./components/ListImages";
+
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div>
+        <ListImages />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
